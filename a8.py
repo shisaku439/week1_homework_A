258d3ec584bb2c8e1b33e41c9e8f02ea0573e590
shisaku439 <shisaku555@gmail.com>
2025-08-21 15:37:29 +0900

    a8 コメントアウト

--- a8.py
+++ a8.py
@@ -2,7 +2,7 @@ users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 for user in users_info:
     print(f"Name: {user[0]}, Age: {user[1]}")
 
-
-users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
-for user, age in users_info:
-    print(f"Name: {user}, Age: {age}")
+# 別解
+# users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
+# for user, age in users_info:
+#     print(f"Name: {user}, Age: {age}")
